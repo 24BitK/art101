0971d163f0ed9d47a8eb10cc8060e100c130ddae
Kali <kamdanie@ucsc.edu>
2023-04-24 14:51:42 -0700

    Lab 4
    -added index
    -moved homepage
    -added psuedocode

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,12 +1,59 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock-Paper_Scissors
+// The implementation of the game
+//Kali Daniels <kamdanie@ucsc.edu> 
+// April 2023
+
+//Outline
+//Creating a Rock-Paper-Scissors game:
+//1. Print 3 options
+//2. Accept user input
+//3. Randomize computer input
+//6. Test of win or draw condition
 
 // Constants
+// Rock > Scissors
+// Paper > Rock
+// Scissors > Paper
 
 // Functions
+// Print Rock, Paper, Scissors
+    // Print Rock, Paper, Scissors
+  //Loop Generate Computers choice
+      //Ask user for Choice
+
+  //Loop Play Game
+    //If User Plays Rock and Computer Chose Rock
+      //Display Try Again
+    //If User Plays Rock and Computer Chose Paper
+      //Display you took the L
+      //Prompt try again
+    //If User Plays Rock and Computer Scissors
+      //Display Congratulations! Player Won!
+      //Prompt play again
+
+    //If User Plays Paper and Computer Paper
+      //Display Try Again
+   //If User Plays Paper and Computer Scissors
+      //Display you took the L
+      //Prompt try again
+    //If User Plays Paper and Computer Rock
+      //Display Congratulations! Player Won!
+      //Prompt play again
+      
+    //If User Plays Scissors and Computer Scissors
+      //Display Try Again
+    //If User Plays Scissors and Computer Rock
+      //Display you took the L
+      //Prompt try again
+    //If User Plays Scissors and Computer Paper
+      //Display Congratulations! Player Won!
+      //Prompt play again
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
+
 function myFunction(param1, param2) {
   // some code here
   // return results;
