fb3e34bc66ce5deff3464418ad65cdeaf35f00dc
Kali <kamdanie@ucsc.edu>
2023-04-26 16:07:15 -0700

    Lab 5
    added js
    changed colors on BC

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Data types and Variables
+// Author: Kali Daniels <kamdanies>
+// Date: 4.26.2023
 
 // Constants
-
+make = "Cadilac";
+model="Escalade";
+color="Black";
+year="2021";
 // Functions
+age = 2023 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year+ "<br>");
+document.writeln("Age: " + age + " " + "years <br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
