a5c2d9f7bf5d3f8d18cc3a24653b32bc7410a865
Kali <kamdanie@ucsc.edu>
2023-05-24 19:08:31 -0700

    lab 13

--- Lab13/js/lab.js
+++ Lab13/js/lab.js
@@ -5,26 +5,36 @@
 //{3: "Fizz", 5: "Buzz", 7: "Boom"}
 //loops over the numbers and outputs the number and the matching text 11 
 //for factors
-function fizzBuzzBoom (maxNums, factorobj) {
-  // iterate over all of out numbers
-    for (var num=0; num<maxNums; num++) {
-      // reset output string
-      var outputStr = "";
-      // iterate over the factors we got from the html
-      for (var factor in factorObj) {
-        // check to see if this num is a multiple of factor
-        if (num % factor == 0) {
-        //now if yes, than add the text to output string
-        outputStr += factorObj[factor];
-        }
-      }
-    }    
-    //now if we have words in outputstr, format it like this " - FizzBuzz!"
-    if (outputstr) {
-    outputstr = " - " + outputstr + "!";
+function fizzBuzzBoom () {
+  // iterate over all of out 200
+    for (var num = 1; num < 200; num++) {
+    var audi = " ";
+    //multiples 7 output
+    if (num % 7 == 0){
+      audi += "Boom ";
+    }//multiples 5 output
+    else if (num % 5 == 0){
+      audi += "Buzz ";
     }
-    outputToPage (num.tostring() + outputStr)
+    //multiples 3 output
+    else if (num % 3 == 0){
+      audi += "Fizz ";
+    } else {
+      audi += num + " ";
+    }
   }
-  function reportError(str){
-  outputEl.innerHTML = "«div class='error'>" + str + "</div»";
-  }
+  return audi;
+
+}
+
+//grab element asssociate with where we wat to store out output
+var results = document.getElementById("output");
+
+//create a new paragraph element and store it within outty
+var outty = document.createElement("p");
+
+// change innerHTML of outty
+outty.innerHTML = fizzBuzzBoom();
+
+//append new paragraph
+results.appendChild(outty);
